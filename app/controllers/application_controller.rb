37070df224e74d43438af4588b73d6b983df3f97
kokezeror <kokinakamichi0617@g.ecc.u-tokyo.ac.jp>
2017-10-20 11:39:15 +0900

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
- def home
- end
-
- def help
- end
-
- def about
- end
+    include SessionsHelper
  
 end
